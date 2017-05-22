d6f4654ecc5931cc73e1b642c3fd94a341a52630
Різник Уляна Андріївна <xxzz@ukr.net>
2017-05-22 14:35:32 +0300

    Iss3786 забрала лишні аргументи

--- backend/views/feedback/create.php
+++ backend/views/feedback/create.php
@@ -6,9 +6,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model common\models\Feedback */
 
-$this->title = Yii::t('backend', 'Create feedback', [
-    'modelClass' => 'Feedback',
-]);
+$this->title = Yii::t('backend', 'Create feedback');
 $this->params['breadcrumbs'][] = ['label' => Yii::t('backend', 'Feedbacks'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
--- backend/views/feedback/update.php
+++ backend/views/feedback/update.php
@@ -5,9 +5,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model common\models\Feedback */
 
-$this->title = Yii::t('backend', 'Update feedback', [
-    'modelClass' => 'Feedback',
-]) . ' ' . $model->name;
+$this->title = Yii::t('backend', 'Update feedback') . ' ' . $model->name;
 $this->params['breadcrumbs'][] = ['label' => Yii::t('backend', 'Feedback'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = Yii::t('backend', 'Update');
